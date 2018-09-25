2b4741314fae3d46ab486bd599aa99dd984110bd
mead1600 <mgyawu0974@bths.edu>
2018-09-25 11:47:30 -0400

    words

--- src/MeadGyawu/Main.java
+++ src/MeadGyawu/Main.java
@@ -1,10 +1,21 @@
 package MeadGyawu;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("alright");
-        System.out.println("words");
+       int randomnum = (int)(10.0*Math.random());
+       System.out.println("What is my number");
+       Scanner answer = new Scanner(System.in);
+       int realVal = answer.nextInt();
+       while(realVal != randomnum)
+       {
+            System.out.println("WRONG STOOPID!!!!! POOGERS");
+            answer = new Scanner(System.in);
+            realVal = answer.nextInt();
+       }
+       System.out.println("YOU WIN PogChamp PogChamp!!!!!!");
     }
 }
