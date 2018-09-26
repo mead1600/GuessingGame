3c50b11ae29d9f0aecd3ec518104d50ea9cb02e7
BT_1N3_12 <mgyawu0974@bths.edu>
2018-09-26 11:41:02 -0400

    psycho crusha

--- src/MeadGyawu/Main.java
+++ src/MeadGyawu/Main.java
@@ -4,7 +4,7 @@ import java.lang.Math;
 
 public class Main {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
 	// write your code here
        int randomnum = (int)(10.0*Math.random());
        System.out.println("What is my number");
@@ -17,5 +17,34 @@ public class Main {
             realVal = answer.nextInt();
        }
        System.out.println("YOU WIN PogChamp PogChamp!!!!!!");
+    }*/
+    public static void main(String[] args){
+        int compGuess = 50;
+        int divsor = 2;
+        int lowerBoundary = -1;
+        int upperBoundary = 101;
+        System.out.println("Is your number " + compGuess);
+        Scanner verifier = new Scanner(System.in);
+        String verifiedResult = verifier.nextLine();
+        while (!(verifiedResult .equals("correct")))
+        {
+            if(verifiedResult.equals("higher"))
+            {
+                lowerBoundary = compGuess;
+                compGuess = compGuess + compGuess/2;
+                System.out.println("Is your number " + compGuess);
+                verifier = new Scanner(System.in);
+                verifiedResult = verifier.nextLine();
+            }
+            if (verifiedResult.equals("lower"))
+            {
+                upperBoundary = compGuess;
+                compGuess = compGuess - compGuess/2;
+                System.out.println("Is your number " + compGuess);
+                verifier = new Scanner(System.in);
+                verifiedResult = verifier.nextLine();
+            }
+        }
+        System.out.println("YAY I WON. I ROCK AND YOU SOCCC");
     }
 }
