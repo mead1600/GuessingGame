86cc9e73954eecd9eacc4ee394cb6e900c33b007
chrisss101 <Crafaniello9535@bths.edu>
2018-09-27 11:31:26 -0400

    follow jliu2882 at github for more exclusive content and code

--- src/MeadGyawu/Main.java
+++ src/MeadGyawu/Main.java
@@ -2,9 +2,12 @@ package MeadGyawu;
 import java.util.Scanner;
 import java.lang.Math;
 
+// Both methods work individually, when the second one isn't commented out, either neither program runs, or only the first program runs.
+// So if you want to use the second program, comment out the first one and bring the second one out of the comments
+
 public class Main {
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
 	// write your code here
        int randomnum = (int)(10.0*Math.random());
        System.out.println("What is my number");
@@ -17,12 +20,12 @@ public class Main {
             realVal = answer.nextInt();
        }
        System.out.println("YOU WIN PogChamp PogChamp!!!!!!");
-    }*/
-    public static void main(String[] args){
+    }
+   /* public static void main(String[] args){
         int compGuess = 50;
-        int divsor = 2;
-        int lowerBoundary = -1;
-        int upperBoundary = 101;
+        int changer = 0;
+        int lowerBoundary = 0;
+        int upperBoundary = 100;
         System.out.println("Is your number " + compGuess);
         Scanner verifier = new Scanner(System.in);
         String verifiedResult = verifier.nextLine();
@@ -31,7 +34,8 @@ public class Main {
             if(verifiedResult.equals("higher"))
             {
                 lowerBoundary = compGuess;
-                compGuess = compGuess + compGuess/2;
+                changer = (upperBoundary - lowerBoundary)/2;
+                compGuess = lowerBoundary + changer;
                 System.out.println("Is your number " + compGuess);
                 verifier = new Scanner(System.in);
                 verifiedResult = verifier.nextLine();
@@ -39,12 +43,13 @@ public class Main {
             if (verifiedResult.equals("lower"))
             {
                 upperBoundary = compGuess;
-                compGuess = compGuess - compGuess/2;
+                changer = (upperBoundary - lowerBoundary)/2;
+                compGuess = upperBoundary - changer;
                 System.out.println("Is your number " + compGuess);
                 verifier = new Scanner(System.in);
                 verifiedResult = verifier.nextLine();
             }
         }
         System.out.println("YAY I WON. I ROCK AND YOU SOCCC");
-    }
+    }*/
 }
