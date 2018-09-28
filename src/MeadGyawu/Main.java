abaf21e8336f39b5361f4c16d6fe60b6cebba187
BT_1N3_12 <mgyawu0974@bths.edu>
2018-09-28 11:25:40 -0400

    Ascend Higher

--- src/MeadGyawu/Main.java
+++ src/MeadGyawu/Main.java
@@ -7,48 +7,50 @@ import java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-       int randomnum = (int)(10.0*Math.random());
-       System.out.println("What is my number");
-       Scanner answer = new Scanner(System.in);
-       int realVal = answer.nextInt();
-       while(realVal != randomnum)
-       {
-            System.out.println("WRONG STOOPID!!!!! POOGERS");
-            answer = new Scanner(System.in);
-            realVal = answer.nextInt();
-       }
-       System.out.println("YOU WIN PogChamp PogChamp!!!!!!");
+        // write your code here
+        System.out.println("Which game do you want to play? choose between 'Guess the computers number' and 'The computer will guess your number'");
+        Scanner choice = new Scanner(System.in);
+        String TheChoice = choice.nextLine();
+        if (TheChoice.equals("Guess the computers number")) {
+            int randomnum = (int) (10.0 * Math.random());
+            System.out.println("What is my number");
+            Scanner answer = new Scanner(System.in);
+            int realVal = answer.nextInt();
+            while (realVal != randomnum) {
+                System.out.println("WRONG STOOPID!!!!! POOGERS");
+                answer = new Scanner(System.in);
+                realVal = answer.nextInt();
+            }
+            System.out.println("YOU WIN PogChamp PogChamp!!!!!!");
+        } else {
+            if(TheChoice.equals("The computer will guess your number")) {
+                int compGuess = 50;
+                int changer = 0;
+                int lowerBoundary = 0;
+                int upperBoundary = 100;
+                System.out.println("Is your number " + compGuess);
+                Scanner verifier = new Scanner(System.in);
+                String verifiedResult = verifier.nextLine();
+                while (!(verifiedResult.equals("correct"))) {
+                    if (verifiedResult.equals("higher")) {
+                        lowerBoundary = compGuess;
+                        changer = (upperBoundary - lowerBoundary) / 2;
+                        compGuess = lowerBoundary + changer;
+                        System.out.println("Is your number " + compGuess);
+                        verifier = new Scanner(System.in);
+                        verifiedResult = verifier.nextLine();
+                    }
+                    if (verifiedResult.equals("lower")) {
+                        upperBoundary = compGuess;
+                        changer = (upperBoundary - lowerBoundary) / 2;
+                        compGuess = upperBoundary - changer;
+                        System.out.println("Is your number " + compGuess);
+                        verifier = new Scanner(System.in);
+                        verifiedResult = verifier.nextLine();
+                    }
+                }
+                System.out.println("YAY I WON. I ROCK AND YOU SOCCC");
+            }
     }
-   /* public static void main(String[] args){
-        int compGuess = 50;
-        int changer = 0;
-        int lowerBoundary = 0;
-        int upperBoundary = 100;
-        System.out.println("Is your number " + compGuess);
-        Scanner verifier = new Scanner(System.in);
-        String verifiedResult = verifier.nextLine();
-        while (!(verifiedResult .equals("correct")))
-        {
-            if(verifiedResult.equals("higher"))
-            {
-                lowerBoundary = compGuess;
-                changer = (upperBoundary - lowerBoundary)/2;
-                compGuess = lowerBoundary + changer;
-                System.out.println("Is your number " + compGuess);
-                verifier = new Scanner(System.in);
-                verifiedResult = verifier.nextLine();
-            }
-            if (verifiedResult.equals("lower"))
-            {
-                upperBoundary = compGuess;
-                changer = (upperBoundary - lowerBoundary)/2;
-                compGuess = upperBoundary - changer;
-                System.out.println("Is your number " + compGuess);
-                verifier = new Scanner(System.in);
-                verifiedResult = verifier.nextLine();
-            }
         }
-        System.out.println("YAY I WON. I ROCK AND YOU SOCCC");
-    }*/
-}
+    }
